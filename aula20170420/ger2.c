7118d7c7517045b0634ce9933bb44742d620cc69
ArthurFB98 <arthur.fborges@hotmail.com>
2017-04-21 15:36:04 -0300

    Update ger2.c

--- aula20170420/ger2.c
+++ aula20170420/ger2.c
@@ -4,12 +4,12 @@ int main()
     int a,soma=0,i;
     printf("Digite um numero: ");
     scanf("%i",&a);
-    for(i=2;i<a;i++)
+    for(i=1;i<a;i++)
         if(a%i==0)
         soma=i+soma;
-        if(soma+1==a)
-            printf("O numero e' perfeito\n");
-        else
+        if(soma!=a || a==0)
             printf("O numero nao e' perfeito\n");
+            else
+            printf("O numero e' perfeito\n");
         return 0;
 }
