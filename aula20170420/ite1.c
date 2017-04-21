23225da3ffe06375bca857b48c01e9176814ad3b
ArthurFB98 <arthur.fborges@hotmail.com>
2017-04-21 15:46:33 -0300

    Update ite1.c

--- aula20170420/ite1.c
+++ aula20170420/ite1.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+void safeFlush()
+{
+    char c;
+    while((c = getchar()) != EOF && c != '\n');
+}
 int main()
 {
     int a,b=0,fat=1;
     printf("Digite um numero: ");
     scanf("%i",&a);
+    safeFlush();
     while(b!=a)
     {
         fat=b*fat+fat;
