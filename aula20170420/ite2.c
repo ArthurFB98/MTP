dd7940d1c15efa5d6b57bafa534de93cae1d499b
ArthurFB98 <arthur.fborges@hotmail.com>
2017-04-21 15:46:59 -0300

    Update ite2.c

--- aula20170420/ite2.c
+++ aula20170420/ite2.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+void safeFlush()
+{
+    char c;
+    while((c = getchar()) != EOF && c != '\n');
+}
 int main()
 {
     int B,P,R=0,aux=1;
     printf("Digite o valor de B e P, respectivamente: ");
     scanf("%i %i",&B,&P);
+    safeFlush();
     while(aux<=P)
     {
         R=B*B+R;
