5620956f13cbc24ee9c5cfee9df007ad5269a79a
ArthurFB98 <arthur.fborges@hotmail.com>
2017-05-06 11:10:54 -0300

    Update ctp3.c

--- aula20170427/ctp3.c
+++ aula20170427/ctp3.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-
 int main ()
 {
     float a,b,c,d,D;
     printf("Digite as coordenada x,y do primeiro e do segundo ponto, respectivamente: ");
     scanf("%f%f%f%f",&a,&b,&c,&d);
     D=sqrt(pow(c-a,2)+pow(d-b,2));
-    printf("A distancia entre os pontos e': %f\n",D);
+    printf("A distancia entre os pontos e': %g\n",D);
     return 0;
 }
