fa7c4a0e4138b9221009dd9ac2241637b1227856
ArthurFB98 <arthur.fborges@hotmail.com>
2017-05-22 23:11:05 -0300

    Update ptr6.c

--- aula20170511/ptr6.c
+++ aula20170511/ptr6.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
 #define N 16
 int main()
 {
-    int i, numero, contagem=0;
-    char frase[N];
-    int *p;
-    for(i=0;i<N;i++) frase[i]=0;
+    int *p,i;
+    char frase[N]={0};
     printf("Digite uma frase: ");
     fgets(frase,N,stdin);
-    p=(int*)&frase;
-    for(i=0;i<sizeof(p);i++)
-    printf("Decimal: %d \nHexadecimal: %X\n"),(p+i),(p+i);
+    p=(int*)frase;
+    printf("\nDecimal: ");
+    for(i=0;i<sizeof(frase)/sizeof(int);i++)
+    printf("%u ",p[i]);
+    printf("\nHexadecimal: ");
+    for(i=0;i<sizeof(frase)/sizeof(int);i++)
+    printf(" %X",p[i]);
     return 0;
 }
