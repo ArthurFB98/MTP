874d60b0142012fa78a1b464948ccad4502f15d7
ArthurFB98 <arthur.fborges@hotmail.com>
2017-05-31 10:04:12 -0300

    Update cacr2.c

--- aula20170525/cacr2.c
+++ aula20170525/cacr2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <complex.h>
 
 struct raizes {
 	float x1;
@@ -14,6 +15,7 @@ float delta(float a, float b, float c) {
 
 float raizes(float a, float b, float c, float delta) {
 	struct raizes raiz;
+	double complex X1,X2;
 	if(delta>0) {
 	raiz.x1=(-b+sqrt(delta))/(2*a);
 	raiz.x2=(-b-sqrt(delta))/(2*a);
@@ -24,8 +26,11 @@ else if(delta==0) {
 	raiz.x2= raiz.x1;
 	printf("As raizes sao: %g e %g",raiz.x1,raiz.x2);
 }
-else
-	printf("Nao tem raiz real");
+else {
+	X1=(-b)/(2*a)+I*(sqrt(-delta))/(2*a);
+	X2=(-b)/(2*a)+I*(sqrt(-delta))/(2*a);
+	printf("As raizes sao: %g+%.4f*i e %g-%.4f*i",creal(X1),cimag(X1),creal(X2),cimag(X2));
+}
 }
 
 int main() {
