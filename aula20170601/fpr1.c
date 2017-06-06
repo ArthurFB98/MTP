0cc3ffb84b63db77bd151b0ad3a33d862fadcd2b
ArthurFB98 <arthur.fborges@hotmail.com>
2017-06-05 21:31:28 -0300

    Update fpr1.c

--- aula20170601/fpr1.c
+++ aula20170601/fpr1.c
@@ -12,6 +12,6 @@ int main() {
     int N1,N2;
     printf("Digite dois numero inteiros: ");
     scanf("%d%d",&N1,&N2);
-    printf("Quociente: %d\nResto: %d",quo(&N1,&N2),res(&N1,&N2));
+    printf("Quociente: %d\nResto: %d\n",quo(&N1,&N2),res(&N1,&N2));
     return 0;
 }
